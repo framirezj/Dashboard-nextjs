ba8b512c2b3776479d37317210d978c0d4708a06
framirezj <12649259+framirezj@users.noreply.github.com>
2025-11-28 22:06:27 -0300

    agrega fonts personalizadas y ocupa el componente Image para cargar las imagenes del dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+
+
+// importamos la fuente personalizada
+import { Inter } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+// especificamos el subconjunto de caracteres
+export const inter = Inter({ subsets: ["latin"], weight: ["400", "700"] });
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400", "700"] });
